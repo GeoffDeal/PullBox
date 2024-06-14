2d168066663296226f003b710ec0b066f550eb5a
Geoff Deal <geoffdeal@proton.me>
2024-06-14 14:56:46 -0230

    Click outside to close flyway

--- src/components/CustomerFlyway.jsx
+++ src/components/CustomerFlyway.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function CustomerFlyway(props) {
     const [isOpen, setIsOpen] = useState(false);
+    const flywayRef = useRef(null);
     const customerId = props.customer;
 
+    const handleOutsideClick = (event) => {
+        if (flywayRef.current && !flywayRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        }
+    }, []);
 
     return(
         <div className="flyawayMenu">
             <button onClick={() => setIsOpen(!isOpen) } className="customerOptions"><span className="material-symbols-outlined">more_horiz</span></button>
-            <div className="customerMenu" style={{display: isOpen ? 'block' : 'none'}}>
+            <div ref={flywayRef} className="customerMenu" style={{display: isOpen ? 'block' : 'none'}}>
                 <button>Customer Details</button>
                 <button>Customer Pulls</button>
                 <button>Remove Customer</button>
