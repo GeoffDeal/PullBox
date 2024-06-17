e4fe0d3be6dcac4692a36372296a965d947e458b
Geoff Deal <geoffdeal@proton.me>
2024-06-17 18:26:24 -0230

    Fixed Home pull display

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,15 @@ import { useContext, useState } from "react";
 function Home () {
     const { user } = useContext(UserContext);
 
+    const now = new Date();
+    const lastSunday = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay());
+
     return (
         <>
             <h1>Welcome {user.name}</h1>
             <h3>Your pulls for this week:</h3>
-            <ComicsDisplay />
+            <ComicsDisplay date={ lastSunday.getTime()} />
             <h3>Upcoming FOCs. Last chance!</h3>
 
         </>
