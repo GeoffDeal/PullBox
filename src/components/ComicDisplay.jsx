eea4bc4e72c712839e6e5d54598103db72af786e
Geoff Deal <geoffdeal@proton.me>
2025-03-31 13:00:54 -0230

    Fixed unique keys for ComicDisplay

--- src/components/ComicDisplay.jsx
+++ src/components/ComicDisplay.jsx
@@ -71,7 +71,7 @@ function ComicsDisplay(props) {
     <div className="bookDisplay">
       <div className="imageContainer">
         {weeksPulls.map((book) => (
-          <NavLink to="/bookpage" state={{ productId: book.ID }} key={book.id}>
+          <NavLink to="/bookpage" state={{ productId: book.ID }} key={book.ID}>
             <img src={book.ImageURL} alt="Comic Cover" />
           </NavLink>
         ))}
