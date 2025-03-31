2707525aaf2da815d51f2998c7dd8ddfff62b31a
Geoff Deal <geoffdeal@proton.me>
2025-03-31 17:01:13 -0230

    Changed Browse and aligned with endpoint

--- src/components/BrowsePage.jsx
+++ src/components/BrowsePage.jsx
@@ -1,123 +1,112 @@
-import { useContext, useEffect } from "react";
+import { useEffect, useState } from "react";
 import SearchDisplay from "./SearchDisplay";
 import WeekSelect from "./WeekSelect";
-import { ComicList } from "../Contexts";
-import { calcWeek } from "./ComicDisplay";
-import { useSearchParams } from "react-router-dom";
-
+import { toast } from "react-toastify";
+import api from "../api/api";
 
 const BrowsePage = () => {
-    const [ searchParams, setSearchParams ] = useSearchParams();
-    const { comics } = useContext(ComicList);
-
-    
-
-    const queryChange = (date) => {
-        setSearchParams(prev => {
-            const updatedParams = new URLSearchParams(prev);
-            updatedParams.set('timestamp', date);
-            return updatedParams;
+  const [date, setDate] = useState(() => {
+    const today = new Date();
+    return today.toLocaleDateString("en-CA");
+  });
+  const [timeframe, setTimeframe] = useState("release");
+  const [product, setProduct] = useState("Comic");
+  const [publisher, setPublisher] = useState("All");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxPages, setMaxPages] = useState(1);
+  const [productList, setProductList] = useState([]);
+  const itemsPerPage = 20;
+  useEffect(() => {
+    let cancelled = false;
+    async function getBrowsed() {
+      try {
+        const res = await api.get("/products/browse", {
+          params: {
+            week: timeframe,
+            date: date,
+            product: product,
+            publisher: publisher,
+            limit: itemsPerPage,
+            page: currentPage,
+          },
         });
-    }
-    const timeframeChange = (event) => {
-        const timeframe = event.target.value;
-        setSearchParams(prev => {
-            const updatedParams = new URLSearchParams(prev);
-            updatedParams.set('timeframe', timeframe);
-            return updatedParams;
-        });
-    }
-    const publisherChange = (event) => {
-        const pubValue = event.target.value;
-        setSearchParams(prev => {
-            const updatedParams = new URLSearchParams(prev);
-            updatedParams.set('publisher', pubValue);
-            return updatedParams;
-        });
-    }
-    const productChange = (event) => {
-        const productValue = event.target.value;
-        setSearchParams(prev => {
-            const updatedParams = new URLSearchParams(prev);
-            updatedParams.set('product', productValue);
-            return updatedParams;
-        });
-    }
-    const pageChange = (pageNumber) => {
-        const page = searchParams.get('page');
-        if (Number(page) !== pageNumber) {
-        setSearchParams(prev => {
-            const updatedParams = new URLSearchParams(prev);
-            updatedParams.set('page', pageNumber);
-            return updatedParams;
-        });
+        if (!cancelled) {
+          setProductList(res.data.data);
+          setMaxPages(res.data.pages);
         }
+      } catch (err) {
+        console.error(err);
+        toast.error("Error retrieving products");
+      }
     }
+    getBrowsed();
+    return () => {
+      cancelled = true;
+    };
+  }, [timeframe, date, product, publisher, currentPage]);
 
-    useEffect(() => {
-        
-        const now = new Date();
-        const lastSunday = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-        lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay());
-        const timestamp = calcWeek(lastSunday);
+  const pageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  const weekChange = (day) => {
+    const formattedDay = day.toLocaleDateString("en-CA");
+    setDate(formattedDay);
+  };
 
-        const defaultParams = new URLSearchParams({
-            timeframe: searchParams.get('timeframe') || 'release',
-            timestamp: searchParams.get('timestamp') || timestamp,
-            product: searchParams.get('product') || 'Comic',
-            publisher: searchParams.get('publisher') || 'All',
-            page: searchParams.get('page') || '1',
-        });
-        setSearchParams(defaultParams);
-    }, []);
+  return (
+    <div className="pageDisplay">
+      <h1>Browse</h1>
+      <p>Browse by: </p>
+      <select
+        value={timeframe}
+        onChange={(event) => {
+          setTimeframe(event.target.value);
+        }}
+      >
+        <option value="release">Release Date</option>
+        <option value="foc">Final Order Date</option>
+        <option value="none">None</option>
+      </select>
 
-    const searchBooks = () => {
-        const timestampNumber = Number(searchParams.get('timestamp'));
-        return comics.filter(product => 
-            (searchParams.get('publisher') === 'All' || product.Publisher === searchParams.get('publisher')) &&
-            (searchParams.get('product') === 'All' || product.ProductType === searchParams.get('product')) &&
-            (searchParams.get('timeframe') !== 'release' || calcWeek(product.Release) === timestampNumber) &&
-            (searchParams.get('timeframe') !== 'foc' || calcWeek(product.FOCDueDate) === timestampNumber)
-        );
-    }
-    const selectedTimeframe = searchParams.get('timeframe') || 'release';
-    const selectedProduct = searchParams.get('product') || 'Comic';
-    const selectedPublisher = searchParams.get('publisher') || 'All';
-
-    const searchedBooks = searchBooks();
-
-    return(
-        <div className="pageDisplay">
-            <h1>Browse</h1>
-            <p>Browse by: </p>
-            <select value={selectedTimeframe} onChange={timeframeChange}>
-                <option value='release'>Release Date</option>
-                <option value='foc'>Final Order Date</option>
-                <option value='none'>None</option>
-            </select>
-            
-            <select value={selectedProduct} onChange={productChange}>
-                <option value={'Comic'}>Comics</option>
-                <option value={'Trade Paperback'}>Trade Paperbacks</option>
-                <option value={'Hardcover'}>Hardcovers</option>
-                <option value={'Graphic Novel'}>Graphic Novels</option>
-                <option value={'Poster'}>Posters</option>
-                <option value={'All'}>All</option>
-            </select>
-            <select value={selectedPublisher} onChange={publisherChange}>
-                <option value={'All'}>All</option>
-                <option value={'Marvel'}>Marvel</option>
-                <option value={'Dc'}>DC</option>
-                <option value={'Image'}>Image</option>
-                <option value={'Dark Horse'}> Dark Horse</option>
-                <option value={'Idw'}>IDW</option>
-                <option value={'Boom!'}>BOOM!</option>
-                <option value={'Dynamite'}>Dynamite</option>
-            </select>
-            {searchParams.get('timeframe') !== 'none' && <WeekSelect onDataPass={queryChange} defaultTime={searchParams.get('timestamp')} />}
-            <SearchDisplay query={ searchedBooks } defaultPage={searchParams.get('page')} onPageChange={pageChange}/>
-        </div>
-    )
-}
+      <select
+        value={product}
+        onChange={(event) => {
+          setProduct(event.target.value);
+        }}
+      >
+        <option value={"Comic"}>Comics</option>
+        <option value={"Trade Paperback"}>Trade Paperbacks</option>
+        <option value={"Hardcover"}>Hardcovers</option>
+        <option value={"Graphic Novel"}>Graphic Novels</option>
+        <option value={"Poster"}>Posters</option>
+        <option value={"All"}>All</option>
+      </select>
+      <select
+        value={publisher}
+        onChange={(event) => {
+          setPublisher(event.target.value);
+        }}
+      >
+        <option value={"All"}>All</option>
+        <option value={"Marvel"}>Marvel</option>
+        <option value={"Dc"}>DC</option>
+        <option value={"Image"}>Image</option>
+        <option value={"Dark Horse"}> Dark Horse</option>
+        <option value={"Idw"}>IDW</option>
+        <option value={"Boom!"}>BOOM!</option>
+        <option value={"Dynamite"}>Dynamite</option>
+      </select>
+      {timeframe !== "none" && (
+        <WeekSelect onDataPass={weekChange} defaultTime={date} />
+      )}
+      <SearchDisplay
+        query={productList}
+        maxPages={maxPages}
+        onPageChange={pageChange}
+        defaultPage={currentPage}
+      />
+    </div>
+  );
+};
 
 export default BrowsePage;
--- src/components/WeekSelect.jsx
+++ src/components/WeekSelect.jsx
@@ -1,36 +1,36 @@
 import { useState } from "react";
-import { calcWeek } from "./ComicDisplay";
 
 const WeekSelect = ({ onDataPass, defaultTime }) => {
-    const now = new Date();
-    const lastSunday = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-    lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay());
-
-    const defaultSunday = defaultTime ? new Date(Number(defaultTime)) : null;
-
-    const [ sunday, setSunday ] = useState(defaultTime ? defaultSunday : lastSunday);
-    const futureWeek = () => {
-        const day = new Date (sunday);
-        day.setDate(sunday.getDate() + 7);
-        setSunday(day);
-        const timestamp = calcWeek(day);
-        onDataPass(timestamp);
-    }
-    const pastWeek = () => {
-        const day = new Date(sunday);
-        day.setDate(sunday.getDate() - 7);
-        setSunday(day);
-        const timestamp = calcWeek(day);
-        onDataPass(timestamp);
-
-    }
-    return(
-        <div className="weekSelector">
-            <button onClick={pastWeek} className="weekSelectors"><span className="material-symbols-outlined">chevron_left</span></button>
-            <p>{ sunday.toDateString() }</p>
-            <button onClick={futureWeek} className="weekSelectors"><span className="material-symbols-outlined">chevron_right</span></button>
-        </div>
-    )
-}
+  const now = new Date();
+  const lastSunday = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay());
+  const defaultSunday = defaultTime ? new Date(defaultTime) : null;
+  const [sunday, setSunday] = useState(
+    defaultTime ? defaultSunday : lastSunday
+  );
+  const futureWeek = () => {
+    const day = new Date(sunday);
+    day.setDate(sunday.getDate() + 7);
+    setSunday(day);
+    onDataPass(day);
+  };
+  const pastWeek = () => {
+    const day = new Date(sunday);
+    day.setDate(sunday.getDate() - 7);
+    setSunday(day);
+    onDataPass(day);
+  };
+  return (
+    <div className="weekSelector">
+      <button onClick={pastWeek} className="weekSelectors">
+        <span className="material-symbols-outlined">chevron_left</span>
+      </button>
+      <p>{sunday.toLocaleDateString("en-CA")}</p>
+      <button onClick={futureWeek} className="weekSelectors">
+        <span className="material-symbols-outlined">chevron_right</span>
+      </button>
+    </div>
+  );
+};
 
 export default WeekSelect;
