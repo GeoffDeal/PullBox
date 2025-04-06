d774fd775a013da5fd714b100a3fe9522b2943c5
Geoff Deal <geoffdeal@proton.me>
2025-04-06 15:35:50 -0230

    Fixed sort functions

--- src/components/ShopPulls.jsx
+++ src/components/ShopPulls.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import WeekSelect from "./WeekSelect";
 import { handleTitle, findSundays } from "../utils/utilityFunctions.js";
 import ShopSubTable from "./ShopSubTable";
@@ -8,24 +8,13 @@ import { toast } from "react-toastify";
 import api from "../api/api.js";
 
 const ShopPulls = () => {
-  // const { customers } = useContext(CustomersContext);
-  // const [pulls, setPulls] = useState([]);
-  // const [weeksPulls, setWeeksPulls] = useState();
   const [sortBy, setSortBy] = useState({
     az: "ascending",
     type: "Publisher",
   });
+  const [pullData, setPullData] = useState();
   const [sortedPulls, setSortedPulls] = useState();
-  // const defaultTimestamp = () => {
-  //   const now = new Date();
-  //   const lastSunday = new Date(
-  //     now.getFullYear(),
-  //     now.getMonth(),
-  //     now.getDate()
-  //   );
-  //   lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay());
-  //   return calcWeek(lastSunday);
-  // };
+
   const { lastSunday } = findSundays();
   const [queryConditions, setQueryConditions] = useState({
     dateType: "release",
@@ -33,9 +22,76 @@ const ShopPulls = () => {
   });
   const [customerAmounts, setCustomerAmounts] = useState([]);
 
+  //Sort functions
+  const sortPulls = useCallback(
+    (productArray, sortConfig = sortBy) => {
+      if (productArray && productArray.length > 0) {
+        const sortedArray = [...productArray];
+        sortedArray.sort((a, b) => {
+          if (a[sortConfig.type] < b[sortConfig.type]) {
+            return sortConfig.az === "ascending" ? -1 : 1;
+          }
+          if (a[sortConfig.type] > b[sortConfig.type]) {
+            return sortConfig.az === "ascending" ? 1 : -1;
+          }
+          return 0;
+        });
+        return sortedArray;
+      } else {
+        return [];
+      }
+    },
+    [sortBy]
+  );
+
+  const toggleAscending = () => {
+    console.log("trigger");
+    setSortBy((prev) => {
+      const newOrder = prev.az === "ascending" ? "descending" : "ascending";
+      const newSortBy = { ...prev, az: newOrder };
+      const newSorted = sortPulls(pullData, newSortBy);
+      setSortedPulls(newSorted);
+      return newSortBy;
+    });
+  };
+  const changeDateType = (e) => {
+    setQueryConditions((prev) => ({
+      ...prev,
+      dateType: e.target.value,
+    }));
+  };
+  const changeDate = (date) => {
+    setQueryConditions((prev) => ({
+      ...prev,
+      date: date,
+    }));
+  };
+
+  const sortPublisher = () => {
+    sortBy.type !== "Publisher"
+      ? setSortBy((prev) => {
+          const newSortBy = { ...prev, type: "Publisher" };
+          const newSorted = sortPulls(pullData, newSortBy);
+          setSortedPulls(newSorted);
+          return newSortBy;
+        })
+      : toggleAscending();
+  };
+  const sortTitle = () => {
+    sortBy.type !== "ProductName"
+      ? setSortBy((prev) => {
+          const newSortBy = { ...prev, type: "ProductName" };
+          const newSorted = sortPulls(pullData, newSortBy);
+          setSortedPulls(newSorted);
+          return newSortBy;
+        })
+      : toggleAscending();
+  };
+
+  // Fetch data
   useEffect(() => {
     let cancelled = false;
-
+    console.log("fetch");
     const getPulls = async () => {
       try {
         const params = { [queryConditions.dateType]: queryConditions.date };
@@ -73,6 +129,7 @@ const ShopPulls = () => {
               }
             });
           }
+          setPullData(combinedBooks);
           setSortedPulls(combinedBooks);
           setCustomerAmounts(customerPulls);
         }
@@ -86,98 +143,6 @@ const ShopPulls = () => {
       cancelled = true;
     };
   }, [queryConditions]);
-
-  // useEffect(() => {
-  //   let totalPulls = [];
-  //   customers.forEach((customer) => {
-  //     const customerPulls = customer.pulls;
-  //     totalPulls = totalPulls.concat(customerPulls);
-  //   });
-  //   setPulls(totalPulls);
-  // }, [customers]);
-
-  // useEffect(() => {
-  //   const weeksBooks = pulls.filter((book) => {
-  //     return (
-  //       calcWeek(
-  //         sortConditions.dateType === "release" ? book.Release : book.FOCDueDate
-  //       ) === sortConditions.timestamp
-  //     );
-  //   });
-  //   setWeeksPulls(weeksBooks);
-  // }, [pulls, sortConditions]);
-
-  // useEffect(() => {
-  //   if (weeksPulls && weeksPulls.length > 0) {
-  //     const bookList = JSON.parse(JSON.stringify(weeksPulls));
-
-  // const combinedBooks = [];
-  // bookList.forEach((book) => {
-  //   //Combine multiples
-  //   if (!combinedBooks.some((comic) => comic.Sku === book.Sku)) {
-  //     book.Customer.Quantity = book["Qty.Ord.OnTime"];
-  //     book.customersList = [book.Customer];
-  //     combinedBooks.push(book);
-  //   } else {
-  //     const comic = combinedBooks.find((comic) => comic.Sku === book.Sku);
-  //     comic["Qty.Ord.OnTime"] =
-  //       comic["Qty.Ord.OnTime"] + book["Qty.Ord.OnTime"];
-  //     book.Customer.Quantity = book["Qty.Ord.OnTime"];
-  //     comic.customersList.push(book.Customer);
-  //   }
-  // });
-  //     combinedBooks.sort((a, b) => {
-  //       if (a[sortBy.type] < b[sortBy.type]) {
-  //         return sortBy.sort === "ascending" ? -1 : 1;
-  //       }
-  //       if (a[sortBy.type] > b[sortBy.type]) {
-  //         return sortBy.sort === "ascending" ? 1 : -1;
-  //       }
-  //       return 0;
-  //     });
-  //     setSortedPulls(combinedBooks);
-  //   } else {
-  //     setSortedPulls([]);
-  //   }
-  // }, [weeksPulls, sortBy, sortConditions]);
-
-  //Sort functions
-
-  const toggleAscending = () => {
-    setSortBy((prev) => ({
-      ...prev,
-      az: prev.az === "ascending" ? "descending" : "ascending",
-    }));
-  };
-  const changeDateType = (e) => {
-    setQueryConditions((prev) => ({
-      ...prev,
-      dateType: e.target.value,
-    }));
-  };
-  const changeDate = (date) => {
-    setQueryConditions((prev) => ({
-      ...prev,
-      date: date,
-    }));
-  };
-
-  const sortPublisher = () => {
-    sortBy.type !== "Publisher"
-      ? setSortBy((prev) => ({
-          ...prev,
-          type: "Publisher",
-        }))
-      : toggleAscending();
-  };
-  const sortTitle = () => {
-    sortBy.type !== "ProductName"
-      ? setSortBy((prev) => ({
-          ...prev,
-          type: "ProductName",
-        }))
-      : toggleAscending();
-  };
 
   // Create excel sheet and populate pulls
 
