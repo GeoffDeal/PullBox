bf266fb95db061b2dcc9d47743d50f216c605f81
Geoff Deal <geoffdeal@proton.me>
2025-04-06 22:06:58 -0230

    Added file upload to endpoint, cleaned up funtion

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,248 +1,237 @@
 import { useContext, useState, useRef } from "react";
-import { StoreInformation, ComicList, TaxRates, ConversionRate, SeriesContext} from "../Contexts";
-import ExcelJS from 'exceljs';
-import { xlsxToObjects, doublesCheck, bookSort, seriesDoubleCheck } from "./BackendFunctions";
-
+import { StoreInformation, TaxRates, ConversionRate } from "../Contexts";
+import { toast } from "react-toastify";
+import api from "../api/api.js";
 
 const AdminPage = () => {
-    const { storeInfo, setStoreInfo } = useContext(StoreInformation);
-    const [ files, setFiles ] = useState('');
-    const [ phone, setPhone ] = useState('');
-    const [ address, setAddress ] = useState('');
-    const [ email, setEmail ] = useState('');
-    const { comics, setComics } = useContext(ComicList);
-    const [ uploadMessage, setUploadMessage] = useState('');
-    const inputRef = useRef();
+  const { storeInfo, setStoreInfo } = useContext(StoreInformation);
+  const [files, setFiles] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const inputRef = useRef();
 
-    const { series, setSeries } = useContext(SeriesContext);
+  //Handle importing excel sheets
 
-    //Hand importing excel sheets
-    const fileChange = (event) => {
-        setFiles(event.target.files);
+  const [isUploading, setIsUploading] = useState(false);
+  const fileChange = (event) => {
+    setFiles(event.target.files);
+  };
+  const handleImport = async (event) => {
+    event.preventDefault();
+    setIsUploading(true);
+    try {
+      if (files.length < 1) throw new Error("Please choose a file");
+      const filesArray = [...files];
+      const formData = new FormData();
+      filesArray.forEach((file) => {
+        formData.append("file", file);
+      });
+      await api.post("/products/upload", formData);
+      toast.success(`Upload Successful!`);
+      inputRef.current.value = null;
+      setFiles("");
+    } catch (err) {
+      toast.error(`Problem with upload: ${err.message}`);
+      console.error(err);
+    } finally {
+      setIsUploading(false);
     }
-    const handleImport = async (event) => {
-        event.preventDefault();
-    
-        const booksArray = [];
-        const seriesArray = [];
-        const filePromises = [];
+  };
 
-        for (let i = 0; i < files.length; i++) {
-            const reader = new FileReader();
+  // Store info change
+  const phoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+  const addressChange = (e) => {
+    setAddress(e.target.value);
+  };
+  const emailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const storeUpdate = (e) => {
+    e.preventDefault();
+    setStoreInfo((prev) => {
+      return {
+        ...prev,
+        ...(phone && { phone }),
+        ...(address && { address }),
+        ...(email && { email }),
+      };
+    });
+  };
 
-            const filePromise = new Promise ((resolve, reject) => {
+  const hourChange = (event) => {
+    const { id, value } = event.target;
+    setStoreInfo((prev) => ({
+      ...prev,
+      hours: {
+        ...prev.hours,
+        [id]: value,
+      },
+    }));
+  };
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-                reader.onload = async (e) => {
-                    const arrayBuffer = e.target.result;
-                    const workbook = new ExcelJS.Workbook();
-                    try {
-                    await workbook.xlsx.load(arrayBuffer); 
-                    }
-                    catch (error){
-                        console.log(error);
-                        setUploadMessage(`There was a problem with the upload`);
-                        setTimeout(() => {
-                            setUploadMessage('')
-                        }, 10000);
-                        return;
-                    }
-                    
-                    workbook.removeWorksheet(2); //Clear useless sheets and rows
-                    workbook.worksheets[0].spliceRows(1, 1);
-        
-                    const worksheetName = workbook.worksheets[0].name;
-                    const firstCut = worksheetName.indexOf('(') + 1;
-                    const secondCut = worksheetName.indexOf(' ', firstCut);
-                    const publisherName = worksheetName.slice(firstCut, secondCut).toLocaleLowerCase();
-                    const capitalName = publisherName.charAt(0).toLocaleUpperCase() + publisherName.slice(1);
-        
-                    const { newBooks, seriesList } = xlsxToObjects(workbook, capitalName);
-                    booksArray.push(...newBooks);
-                    seriesArray.push(...seriesList);
+  // Tax rate functions
+  const productTypes = [
+    "Hardcover",
+    "Omnibus",
+    "Trade Paperback",
+    "Comic",
+    "Box Set",
+    "Graphic Novel",
+    "Poster",
+    "Incentive",
+  ];
+  const [product, setProduct] = useState("Hardcover");
+  const [rate, setRate] = useState("");
+  const { taxRates, setTaxRates } = useContext(TaxRates);
 
-                    resolve();
-                }
-                try {
-                    reader.readAsArrayBuffer(files[i]); 
-                }
-                catch(error) {
-                    console.log(error);
-                    setUploadMessage(`There was a problem with the upload`);
-                    setTimeout(() => {
-                        setUploadMessage('')
-                    }, 10000);
-                    reject(error);
-                }
-            });
-            filePromises.push(filePromise);
-        }
+  const updateRates = (e) => {
+    e.preventDefault();
+    setTaxRates((prev) => ({
+      ...prev,
+      [product]: rate,
+    }));
+    setRate("");
+  };
 
-        await Promise.all(filePromises);
+  // Conversion rate
+  const { conversion, setConversion } = useContext(ConversionRate);
 
-        const updatedList = doublesCheck(booksArray, comics);
-        const sortedList = bookSort(booksArray.concat(updatedList));
-        setComics(sortedList);
+  const changeConversion = (e) => {
+    setConversion(e.target.value);
+  };
 
-        const updatedSeries = seriesDoubleCheck(seriesArray, series);
-        setSeries(updatedSeries);
-
-        
-        inputRef.current.value = '';
-        setUploadMessage('Uploaded!');
-        setTimeout(() => {
-            setUploadMessage(' ')
-        }, 2000);
-    }
-
-    // Store info change
-    const phoneChange = (e) => {
-        setPhone(e.target.value);
-    }
-    const addressChange = (e) => {
-        setAddress(e.target.value);
-    }
-    const emailChange = (e) => {
-        setEmail(e.target.value);
-    }
-    const storeUpdate = (e) => {
-        e.preventDefault();
-        setStoreInfo((prev) => {
-            return {
-                ...prev,
-                ...(phone && { phone }), 
-                ...(address && { address }),
-                ...(email && { email }),
-            }
-        })
-    }
-
-    const hourChange = (event) => {
-        const { id, value } = event.target;
-        setStoreInfo(prev => ({
-            ...prev,
-            hours: {
-                ...prev.hours,
-                [id]: value,
-            }
-        }));
-    }
-    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',];
-
-    // Tax rate functions
-    const productTypes = ['Hardcover', 'Omnibus', 'Trade Paperback', 'Comic', 'Box Set', 'Graphic Novel', 'Poster', 'Incentive'];
-    const [ product, setProduct ] = useState('Hardcover');
-    const [ rate, setRate ] = useState('');
-    const { taxRates, setTaxRates } = useContext(TaxRates);
-
-    const updateRates = (e) => {
-        e.preventDefault();
-        setTaxRates(prev => ({
-            ...prev,
-            [product]: rate,
-        }));
-        setRate('');
-    }
-
-    // Conversion rate
-    const { conversion, setConversion } = useContext(ConversionRate);
-    
-    const changeConversion = (e) => {
-        setConversion(e.target.value);
-    }
-
-    return (
-        <div className="adminPage pageDisplay">
-            <h1>Store Admin</h1>
-            <div className="fullscreenDiv">
-                <h3>Import Universal Files</h3>
-                <div className="fullscreenFlex" id="fileImport">
-                    <form onSubmit={handleImport} >
-                        <input type="file" onChange={fileChange} ref={inputRef} multiple/>
-                        <input type="submit" value="Upload" />
-                    </form>
-                    <p id="uploadMessage">{ uploadMessage }</p>
-                </div>
-            </div>
-            <div className="fullscreenFlex">
-                <div className="fullscreenDiv">
-                    <h3>Change Store Info</h3>
-                        <form onSubmit={storeUpdate} id="storeInfoForm">
-                            <div className="infoBlock">
-                                <label htmlFor="phoneInput">Phone: </label>
-                                <input 
-                                    id="phoneInput"
-                                    type="tel"
-                                    onChange={phoneChange}
-                                /> 
-                            </div>
-                            <div className="infoBlock">
-                                <label htmlFor="addressInput">Address: </label>
-                                <input 
-                                    id="addressInput"
-                                    type="text"
-                                    onChange={addressChange}
-                                />
-                            </div>
-                            <div className="infoBlock">
-                                <label htmlFor="emailInput">Email: </label>
-                                <input 
-                                    id="emailInput"
-                                    type="text"
-                                    onChange={emailChange}
-                                />
-                            </div>
-                            <button type="submit">Update</button>
-                        </form>
-                    <h3>Change Store Hours</h3>
-                    <form className="timeForm">
-                        {weekdays.map(day => {
-                            const openKey = day.toLowerCase() + 'open';
-                            const closeKey = day.toLowerCase() + 'close';
-                            return(
-                                <div key={day} className="dayTimes">
-                                    <p className="dayName">{day}</p>
-                                    <label htmlFor={openKey}>Open: </label>
-                                    <input 
-                                        id={openKey} 
-                                        value={storeInfo.hours[openKey]} 
-                                        type="time"
-                                        onChange={hourChange} />
-                                    <label htmlFor={closeKey}>Close: </label>
-                                    <input 
-                                        id={closeKey}
-                                        value={storeInfo.hours[closeKey]} 
-                                        type="time"
-                                        onChange={hourChange} />
-                                </div>
-                            )
-                        })}
-                    </form>
-                </div>
-                <div className="fullscreenDiv">
-                    <h3>Set Tax Rate</h3>
-                    <ul>
-                        {taxRates && Object.keys(taxRates).map((key, index) => {
-                            return <li key={index}>{ key }: { taxRates[key] }%</li>
-                        })}
-                    </ul>
-                    <form onSubmit={(e) => {updateRates(e)}}>
-                        <select onChange={(e) => {setProduct(e.target.value)}}>
-                            {productTypes.map((type, index) => {
-                                return <option key={index} value={ type }>{ type }</option>
-                            })}
-                        </select>
-                        <div className="taxRateDiv">
-                            <input type="number" onChange={(e) => {setRate(e.target.value)}} value={rate}/><p>%</p>
-                        </div>
-                        <button type="submit">Update</button>
-                    </form>
-                    <h3>Set Coversion Rate</h3>
-                    <label htmlFor="conversionInput">Conversion rate from USD: </label>
-                    <input type="number" id="conversionInput" onChange={(e) => changeConversion(e)}/>
-                    <p>Current conversion: { conversion }</p> 
-                </div>
-            </div>
+  return (
+    <div className="adminPage pageDisplay">
+      <h1>Store Admin</h1>
+      <div className="fullscreenDiv">
+        <h3>Import Universal Files</h3>
+        <div className="fullscreenFlex" id="fileImport">
+          <form onSubmit={handleImport}>
+            <input
+              type="file"
+              onChange={fileChange}
+              disabled={isUploading}
+              ref={inputRef}
+              multiple
+            />
+            <input
+              type="submit"
+              disabled={isUploading}
+              value={isUploading ? "Uploading..." : "Upload"}
+            />
+          </form>
         </div>
-    )
-}
+      </div>
+      <div className="fullscreenFlex">
+        <div className="fullscreenDiv">
+          <h3>Change Store Info</h3>
+          <form onSubmit={storeUpdate} id="storeInfoForm">
+            <div className="infoBlock">
+              <label htmlFor="phoneInput">Phone: </label>
+              <input id="phoneInput" type="tel" onChange={phoneChange} />
+            </div>
+            <div className="infoBlock">
+              <label htmlFor="addressInput">Address: </label>
+              <input id="addressInput" type="text" onChange={addressChange} />
+            </div>
+            <div className="infoBlock">
+              <label htmlFor="emailInput">Email: </label>
+              <input id="emailInput" type="text" onChange={emailChange} />
+            </div>
+            <button type="submit">Update</button>
+          </form>
+          <h3>Change Store Hours</h3>
+          <form className="timeForm">
+            {weekdays.map((day) => {
+              const openKey = day.toLowerCase() + "open";
+              const closeKey = day.toLowerCase() + "close";
+              return (
+                <div key={day} className="dayTimes">
+                  <p className="dayName">{day}</p>
+                  <label htmlFor={openKey}>Open: </label>
+                  <input
+                    id={openKey}
+                    value={storeInfo.hours[openKey]}
+                    type="time"
+                    onChange={hourChange}
+                  />
+                  <label htmlFor={closeKey}>Close: </label>
+                  <input
+                    id={closeKey}
+                    value={storeInfo.hours[closeKey]}
+                    type="time"
+                    onChange={hourChange}
+                  />
+                </div>
+              );
+            })}
+          </form>
+        </div>
+        <div className="fullscreenDiv">
+          <h3>Set Tax Rate</h3>
+          <ul>
+            {taxRates &&
+              Object.keys(taxRates).map((key, index) => {
+                return (
+                  <li key={index}>
+                    {key}: {taxRates[key]}%
+                  </li>
+                );
+              })}
+          </ul>
+          <form
+            onSubmit={(e) => {
+              updateRates(e);
+            }}
+          >
+            <select
+              onChange={(e) => {
+                setProduct(e.target.value);
+              }}
+            >
+              {productTypes.map((type, index) => {
+                return (
+                  <option key={index} value={type}>
+                    {type}
+                  </option>
+                );
+              })}
+            </select>
+            <div className="taxRateDiv">
+              <input
+                type="number"
+                onChange={(e) => {
+                  setRate(e.target.value);
+                }}
+                value={rate}
+              />
+              <p>%</p>
+            </div>
+            <button type="submit">Update</button>
+          </form>
+          <h3>Set Coversion Rate</h3>
+          <label htmlFor="conversionInput">Conversion rate from USD: </label>
+          <input
+            type="number"
+            id="conversionInput"
+            onChange={(e) => changeConversion(e)}
+          />
+          <p>Current conversion: {conversion}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default AdminPage;
